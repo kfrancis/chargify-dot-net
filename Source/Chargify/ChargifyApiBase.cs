f5284073f2d033210ff734e67f6bba7525b4bee3
Kori Francis <byter@me.com>
2015-06-01 16:29:30 -0400

    Start refactor for async

--- Source/Chargify/ChargifyApiBase.cs
+++ Source/Chargify/ChargifyApiBase.cs
@@ -23,7 +23,7 @@ namespace Chargify
             {
                 if (_userAgent == null)
                 {
-                    _userAgent = String.Format("Chargify .NET RestSharp Client v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
+                    _userAgent = string.Format(format: "Chargify .NET RestSharp Client v{0}", arg0: System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
                 }
                 return _userAgent;
             }
@@ -36,25 +36,26 @@ namespace Chargify
         protected ChargifyApiBase(string apiKey, string apiPassword)
         {
             if (string.IsNullOrEmpty(apiKey))
-                throw new ArgumentException("Api key is required. Details can be found at http://docs.chargify.com/api-authentication", "apiKey");
+                throw new ArgumentException(message: "Api key is required. Details can be found at http://docs.chargify.com/api-authentication", paramName: "apiKey");
 
-            this.ApiKey = apiKey;
-            this.ApiPassword = apiPassword;
-            this.UseJSON = false;
+            ApiKey = apiKey;
+            ApiPassword = apiPassword;
+            UseJSON = false;
 
             _client = new RestClient();
-            _client.Authenticator = new HttpBasicAuthenticator(this.ApiKey, this.ApiPassword);
-            _client.UserAgent = ChargifyApiBase.UserAgent;
-            _client.BaseUrl = Config.ApiBaseUrl;
+            _client.Authenticator = new HttpBasicAuthenticator(ApiKey, ApiPassword);
+            _client.UserAgent = UserAgent;
+            _client.BaseUrl = new Uri(Config.ApiBaseUrl);
         }
 
         protected ChargifyApiBase(string apiKey, string apiPassword, bool useJson)
             : this(apiKey, apiPassword)
         {
-            this.UseJSON = useJson;
+            UseJSON = useJson;
         }
         #endregion
 
+        #region Sync
         protected T GetRequest<T>(string path, params object[] args) where T : new()
         {
             return GetRequest<T>(path, string.Empty, args);
@@ -70,11 +71,11 @@ namespace Chargify
 
             if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
             {
-                throw new ChargifyException("Not Found");
+                throw new ChargifyException(message: "Not Found");
             }
             else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
             {
-                throw new ChargifyException("Internal Server Error");
+                throw new ChargifyException(message: "Internal Server Error");
             }
 
             return response.Data;
@@ -92,7 +93,7 @@ namespace Chargify
 
         protected void DeleteRequest(string path, params object[] args)
         {
-            Request<List<object>, object>(Method.DELETE, null, path, args);
+            Request<List<object>, object>(method: Method.DELETE, obj: null, path: path, args: args);
         }
 
         private TOutput Request<TOutput, TInput>(Method method, TInput obj, string path, params object[] args) where TOutput : new()
@@ -109,22 +110,100 @@ namespace Chargify
 
             if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
             {
-                throw new ChargifyException("Not Found");
+                throw new ChargifyException(message: "Not Found");
             }
             else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
             {
-                throw new ChargifyException("Internal Server Error");
+                throw new ChargifyException(message: "Internal Server Error");
             }
 
             return response.Data;
         }
+        #endregion
 
+        #region Async
+        protected async Task<T> GetRequestAsync<T>(string path, params object[] args) where T : new()
+        {
+            return await GetRequestAsync<T>(path, string.Empty, args).ConfigureAwait(continueOnCapturedContext: false);
+        }
+
+        protected Task<T> GetRequestAsync<T>(string path, string rootElement, params object[] args) where T : new()
+        {
+            RestRequest request = new RestRequest(BuildUrl(path, args));
+            if (!string.IsNullOrWhiteSpace(rootElement)) request.RootElement = rootElement;
+            SetupRequest(request);
+
+            var tcs = new TaskCompletionSource<T>();
+            _client.ExecuteAsync<T>(request, (response)=>{
+                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    tcs.SetException(new ChargifyException(message: "Not Found"));
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+                {
+                    tcs.SetException(new ChargifyException(message: "Internal Server Error"));
+                }
+                else
+                {
+                    tcs.SetResult(response.Data);
+                }
+            });
+
+            return tcs.Task;
+        }
+
+        protected async Task<TOutput> PutRequestAsync<TOutput, TInput>(TInput obj, string path, params object[] args) where TOutput : new()
+        {
+            return await RequestAsync<TOutput, TInput>(Method.PUT, obj, path, args).ConfigureAwait(continueOnCapturedContext: false);
+        }
+
+        protected async Task<TOutput> PostRequestAsync<TOutput, TInput>(TInput obj, string path, params object[] args) where TOutput : new()
+        {
+            return await RequestAsync<TOutput, TInput>(Method.POST, obj, path, args).ConfigureAwait(continueOnCapturedContext: false);
+        }
+
+        protected async Task DeleteRequestAsync(string path, params object[] args)
+        {
+            await RequestAsync<List<object>, object>(method: Method.DELETE, obj: null, path: path, args: args).ConfigureAwait(continueOnCapturedContext: false);
+        }
+
+        private Task<TOutput> RequestAsync<TOutput, TInput>(Method method, TInput obj, string path, params object[] args) where TOutput : new()
+        {
+            RestRequest request = new RestRequest(BuildUrl(path, args), method);
+            SetupRequest(request);
+
+            if (obj != null)
+            {
+                request.AddBody(obj);
+            }
+
+            var tcs = new TaskCompletionSource<TOutput>();
+            _client.ExecuteAsync<TOutput>(request, (response) => {
+                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    tcs.SetException(new ChargifyException(message: "Not Found"));
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+                {
+                    tcs.SetException(new ChargifyException(message: "Internal Server Error"));
+                }
+                else
+                {
+                    tcs.SetResult(response.Data);
+                }
+            });
+
+            return tcs.Task;
+        }
+        #endregion
+
+        #region Helper Methods
         protected string BuildUrl(string path, params object[] args)
         {
             // Assume for now that no querystring is added
             path = string.Format(path, args);
 
-            if (this.UseJSON)
+            if (UseJSON)
             {
                 path += ".json";
             }
@@ -138,7 +217,7 @@ namespace Chargify
 
         protected void SetupRequest(RestRequest request)
         {
-            if (request.Resource.ToLowerInvariant().Contains(".xml"))
+            if (request.Resource.ToLowerInvariant().Contains(value: ".xml"))
             {
                 request.RequestFormat = DataFormat.Xml;
                 request.XmlSerializer = new RestSharp.Serializers.DotNetXmlSerializer();
@@ -149,5 +228,6 @@ namespace Chargify
                 request.JsonSerializer = new JsonSerializer();
             }
         }
+        #endregion
     }
 }
--- Source/Chargify/Products/ProductService.cs
+++ Source/Chargify/Products/ProductService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Chargify
 {
-    public class ProductService : ChargifyApiBase
+    public partial class ProductService : ChargifyApiBase, IProductService
     {
         public static readonly string ProductKey = "products";
 
@@ -16,38 +16,88 @@ namespace Chargify
 
         public IEnumerable<Product> All()
         {
-            string url = string.Format("/{0}", ProductService.ProductKey);
+            string url = string.Format(format: "/{0}", arg0: ProductKey);
             return GetRequest<List<Product>>(url);
         }
 
         public IEnumerable<Product> All(int familyId)
         {
-            string url = string.Format("/{0}/{1}/{2}", ProductFamilyService.ProductFamilyKey, familyId, ProductService.ProductKey);
+            string url = string.Format(format: "/{0}/{1}/{2}", arg0: ProductFamilyService.ProductFamilyKey, arg1: familyId, arg2: ProductKey);
             return GetRequest<List<Product>>(url);
         }
 
         public Product Single(int id)
         {
-            string url = string.Format("/{0}/{1}", ProductService.ProductKey, id);
+            string url = string.Format(format: "/{0}/{1}", arg0: ProductKey, arg1: id);
             return GetRequest<Product>(url);
         }
 
         public Product Single(string handle)
         {
-            string url = string.Format("/{0}/handle/{1}", ProductService.ProductKey, handle);
+            string url = string.Format(format: "/{0}/handle/{1}", arg0: ProductKey, arg1: handle);
             return GetRequest<Product>(url);
         }
 
         public Product Create(int familyId, Product product)
         {
-            string url = string.Format("/{0}/{1}/{2}", ProductFamilyService.ProductFamilyKey, familyId, ProductService.ProductKey);
+            string url = string.Format(format: "/{0}/{1}/{2}", arg0: ProductFamilyService.ProductFamilyKey, arg1: familyId, arg2: ProductKey);
             return PostRequest<Product, Product>(product, url);
         }
 
         public void Archive(Product product)
         {
             throw new NotImplementedException();
-            //DeleteRequest("/product_families/{0}/products/{1}", product.product_family.id, product.id);
         }
+
+        public Task<IEnumerable<Product>> AllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> AllAsync(int familyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> SingleAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> SingleAsync(string handle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> CreateAsync(int familyId, Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ArchiveAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IProductService
+    {
+        IEnumerable<Product> All();
+        Task<IEnumerable<Product>> AllAsync();
+
+        IEnumerable<Product> All(int familyId);
+        Task<IEnumerable<Product>> AllAsync(int familyId);
+
+        Product Single(int id);
+        Task<Product> SingleAsync(int id);
+
+        Product Single(string handle);
+        Task<Product> SingleAsync(string handle);
+
+        Product Create(int familyId, Product product);
+        Task<Product> CreateAsync(int familyId, Product product);
+
+        void Archive(Product product);
+        Task ArchiveAsync(Product product);
     }
 }
