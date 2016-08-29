e2ceddec179682f8d4afc5c7f36283e1b5934ef2
Kori Francis <byter@me.com>
2016-08-29 14:32:48 -0400

    .NET Core wip

---
+++ Source/ChargifyNET.DotCoreTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using ChargifyNET.DotCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChargifyNET.DotCoreTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            chargify.Test();
+        }
+    }
+}
