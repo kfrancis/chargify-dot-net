7c683aff7f931655d13efb51e3b1a610ab551f33
Kori Francis <byter@me.com>
2017-07-25 11:43:32 -0400

    Internalize PCLWebUtility (for now) and update Json.Net

---
+++ Source/ChargifyDotNet/PCLWebUtility/UrlUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCLWebUtility
+{
+    internal class UrlUtility
+    {
+        public UrlUtility()
+        {
+        }
+
+        public static string UrlDecode(string text)
+        {
+            text = text.Replace("+", " ");
+            return Uri.UnescapeDataString(text);
+        }
+
+        public static string UrlEncode(string text)
+        {
+            return Uri.EscapeDataString(text);
+        }
+    }
+}
