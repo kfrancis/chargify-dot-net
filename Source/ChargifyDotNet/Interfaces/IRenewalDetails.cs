68f12c34e2e7b55b93f29785a76d9667ac77dfa7
Kori Francis <kfrancis@clinicalsupportsystems.com>
2021-11-03 13:42:46 -0400

    Fix renewal details implementation

--- Source/ChargifyDotNet/Interfaces/IRenewalDetails.cs
+++ Source/ChargifyDotNet/Interfaces/IRenewalDetails.cs
@@ -1,11 +1,11 @@
-﻿
 #region License, Terms and Conditions
+
 //
 // IRenewalDetails.cs
 //
 // Authors: Kori Francis <twitter.com/djbyter>, David Ball
 // Copyright (C) 2010 Clinical Support Systems, Inc. All rights reserved.
-// 
+//
 //  THIS FILE IS LICENSED UNDER THE MIT LICENSE AS OUTLINED IMMEDIATELY BELOW:
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a
@@ -26,18 +26,21 @@
 //  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 //  DEALINGS IN THE SOFTWARE.
 //
-#endregion
 
-using System;
-using System.Collections.Generic;
-using System.Xml;
-using System.Xml.Serialization;
-using ChargifyNET.Json;
-using Newtonsoft.Json;
+#endregion
 
 // ReSharper disable once CheckNamespace
 namespace ChargifyNET
 {
+    #region Imports
+    using System;
+    using System.Collections.Generic;
+    using System.Xml;
+    using System.Xml.Serialization;
+    using ChargifyNET.Json;
+    using Newtonsoft.Json;
+    #endregion
+
     /// <summary>
     /// The output of a renewal preview
     /// </summary>
@@ -116,8 +119,8 @@ namespace ChargifyNET
         decimal TotalAmountDue { get; }
 
         /// <summary>
-        /// A boolean indicating whether or not additional taxes will be calculated at the time of renewal. 
-        /// This will be true if you are using Avalara and the address of the subscription is 
+        /// A boolean indicating whether or not additional taxes will be calculated at the time of renewal.
+        /// This will be true if you are using Avalara and the address of the subscription is
         /// in one of your defined taxable regions.
         /// </summary>
         [XmlElement("uncalculated_taxes"), JsonProperty("uncalculated_taxes")]
@@ -138,11 +141,91 @@ namespace ChargifyNET
     [XmlRoot("line_item")]
     public class RenewalLineItem
     {
+        private const string TransactionTypeKey = "transaction_type";
+        private const string AmountInCentsKey = "amount_in_cents";
+        private const string DiscountAmountInCentsKey = "discount_amount_in_cents";
+        private const string KindKey = "kind";
+        private const string MemoKey = "memo";
+        private const string ProductHandleKey = "product_handle";
+        private const string ProductIdKey = "product_id";
+        private const string ProductNameKey = "product_name";
+        private const string TaxableAmountInCentsKey = "taxable_amount_in_cents";
+
         #region Constructors
+
         /// <summary>
         /// Default constructor
         /// </summary>
-        public RenewalLineItem() { }
+        public RenewalLineItem()
+        { }
+
+        public RenewalLineItem(string xml)
+            : base()
+        {
+            // get the XML into an XML document
+            XmlDocument Doc = new();
+            Doc.LoadXml(xml);
+            if (Doc.ChildNodes.Count == 0) throw new ArgumentException("XML not valid", nameof(xml));
+            // loop through the child nodes of this node
+            foreach (XmlNode elementNode in Doc.ChildNodes)
+            {
+                if (elementNode.Name == "referral-code")
+                {
+                    this.LoadFromNode(elementNode);
+                    return;
+                }
+            }
+            // if we get here, then no info was found
+            throw new ArgumentException("XML does not contain expected data", nameof(xml));
+        }
+
+        private void LoadFromNode(XmlNode elementNode)
+        {
+            foreach (XmlNode dataNode in elementNode.ChildNodes)
+            {
+                switch (dataNode.Name)
+                {
+                    case AmountInCentsKey:
+                        AmountInCents = dataNode.GetNodeContentAsInt();
+                        break;
+
+                    case DiscountAmountInCentsKey:
+                        DiscountAmountInCents = dataNode.GetNodeContentAsInt();
+                        break;
+
+                    case KindKey:
+                        Kind = dataNode.GetNodeContentAsString();
+                        break;
+
+                    case MemoKey:
+                        Memo = dataNode.GetNodeContentAsString();
+                        break;
+
+                    case ProductHandleKey:
+                        ProductHandle = dataNode.GetNodeContentAsString();
+                        break;
+
+                    case ProductIdKey:
+                        ProductId = dataNode.GetNodeContentAsLong();
+                        break;
+
+                    case ProductNameKey:
+                        ProductName = dataNode.GetNodeContentAsString();
+                        break;
+
+                    case TaxableAmountInCentsKey:
+                        TaxableAmountInCents = dataNode.GetNodeContentAsInt();
+                        break;
+
+                    case TransactionTypeKey:
+                        TransactionType = dataNode.GetNodeContentAsTransactionType();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
 
         /// <summary>
         /// Xml parsing constructor
@@ -150,15 +233,10 @@ namespace ChargifyNET
         /// <param name="node"></param>
         public RenewalLineItem(XmlNode node)
         {
-            // Deserialize
-            var obj = node.ConvertNode<RenewalLineItem>();
-
-            TransactionType = obj.TransactionType;
-            Kind = obj.Kind;
-            AmountInCents = obj.AmountInCents;
-            Memo = obj.Memo;
-            DiscountAmountInCents = obj.DiscountAmountInCents;
-            TaxableAmountInCents = obj.TaxableAmountInCents;
+            if (node == null) throw new ArgumentNullException(nameof(node));
+            if (node.Name != "line_item") throw new ArgumentException("Not a vaild xml node", nameof(node));
+            if (node.ChildNodes.Count == 0) throw new ArgumentException("XML not valid", nameof(node));
+            this.LoadFromNode(node);
         }
 
         /// <summary>
@@ -167,69 +245,127 @@ namespace ChargifyNET
         /// <param name="renewalLineItem"></param>
         public RenewalLineItem(JsonObject renewalLineItem)
         {
-            // Deserialize
-            var obj = JsonConvert.DeserializeObject<RenewalLineItem>(renewalLineItem.ToString());
-
-            TransactionType = obj.TransactionType;
-            Kind = obj.Kind;
-            AmountInCents = obj.AmountInCents;
-            Memo = obj.Memo;
-            DiscountAmountInCents = obj.DiscountAmountInCents;
-            TaxableAmountInCents = obj.TaxableAmountInCents;
+            if (renewalLineItem == null) throw new ArgumentNullException(nameof(renewalLineItem));
+            if (renewalLineItem.Keys.Count <= 0) throw new ArgumentException("Not a vaild renewal line object", nameof(renewalLineItem));
+            this.LoadFromJSON(renewalLineItem);
         }
+
+        private void LoadFromJSON(JsonObject obj)
+        {
+            // loop through the keys of this JsonObject to get referral code info, and parse it out
+            foreach (var key in obj.Keys)
+            {
+                switch (key)
+                {
+                    case AmountInCentsKey:
+                        AmountInCents = obj.GetJSONContentAsInt(key);
+                        break;
+
+                    case DiscountAmountInCentsKey:
+                        DiscountAmountInCents = obj.GetJSONContentAsInt(key);
+                        break;
+
+                    case KindKey:
+                        Kind = obj.GetJSONContentAsString(key);
+                        break;
+
+                    case MemoKey:
+                        Memo = obj.GetJSONContentAsString(key);
+                        break;
+
+                    case ProductHandleKey:
+                        ProductHandle = obj.GetJSONContentAsString(key);
+                        break;
+
+                    case ProductIdKey:
+                        ProductId = obj.GetJSONContentAsInt(key);
+                        break;
+
+                    case ProductNameKey:
+                        ProductName = obj.GetJSONContentAsString(key);
+                        break;
+
+                    case TaxableAmountInCentsKey:
+                        TaxableAmountInCents = obj.GetJSONContentAsInt(key);
+                        break;
+
+                    case TransactionTypeKey:
+                        TransactionType = obj.GetJSONContentAsTransactionType(key);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
         #endregion
 
         #region Properties
+
         /// <summary>
         /// The type of transaction
         /// </summary>
-        [XmlElement("transaction_type"), JsonProperty("transaction_type")]
-        public string TransactionType { get; set; }
+        [XmlElement(TransactionTypeKey), JsonProperty(TransactionTypeKey)]
+        public TransactionType TransactionType { get; set; }
 
         /// <summary>
         /// The kind of transaction
         /// </summary>
-        [XmlElement("kind"), JsonProperty("kind")]
+        [XmlElement(KindKey), JsonProperty(KindKey)]
         public string Kind { get; set; }
 
         /// <summary>
         /// The amount of the transaction in cents
         /// </summary>
-        [XmlElement("amount_in_cents"), JsonProperty("amount_in_cents")]
+        [XmlElement(AmountInCentsKey), JsonProperty(AmountInCentsKey)]
         public int AmountInCents { get; set; }
 
         /// <summary>
         /// The amount of the transaction in dollars and cents
         /// </summary>
-        public decimal Amount { get { return Convert.ToDecimal(AmountInCents) / 100; } }
+        public decimal Amount
+        { get { return Convert.ToDecimal(AmountInCents) / 100; } }
 
         /// <summary>
         /// The memo of the transaction
         /// </summary>
-        [XmlElement("memo"), JsonProperty("memo")]
+        [XmlElement(MemoKey), JsonProperty(MemoKey)]
         public string Memo { get; set; }
 
         /// <summary>
         /// The discount amount in cents
         /// </summary>
-        [XmlElement("discount_amount_in_cents"), JsonProperty("discount_amount_in_cents")]
+        [XmlElement(DiscountAmountInCentsKey), JsonProperty(DiscountAmountInCentsKey)]
         public int DiscountAmountInCents { get; set; }
 
         /// <summary>
         /// The discount amount
         /// </summary>
-        public decimal DiscountAmount { get { return Convert.ToDecimal(DiscountAmountInCents) / 100; } }
+        public decimal DiscountAmount
+        { get { return Convert.ToDecimal(DiscountAmountInCents) / 100; } }
 
         /// <summary>
         /// The taxable amount in cents
         /// </summary>
-        [XmlElement("taxable_amount_in_cents"), JsonProperty("taxable_amount_in_cents")]
+        [XmlElement(TaxableAmountInCentsKey), JsonProperty(TaxableAmountInCentsKey)]
         public int TaxableAmountInCents { get; set; }
 
         /// <summary>
         /// The taxable amount
         /// </summary>
-        public decimal TaxableAmount { get { return Convert.ToDecimal(TaxableAmountInCents) / 100; } }
+        public decimal TaxableAmount
+        { get { return Convert.ToDecimal(TaxableAmountInCents) / 100; } }
+
+        [XmlElement(ProductHandleKey), JsonProperty(ProductHandleKey)]
+        public string ProductHandle { get; set; }
+
+        [XmlElement(ProductIdKey), JsonProperty(ProductIdKey)]
+        public long ProductId { get; set; }
+
+        [XmlElement(ProductNameKey), JsonProperty(ProductNameKey)]
+        public string ProductName { get; set; }
+
         #endregion
     }
 }
