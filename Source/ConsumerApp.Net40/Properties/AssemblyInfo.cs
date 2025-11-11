b06e7a2739eebd0f526801a99fe9f74d3695ad7e
Kori Francis <kfrancis@clinicalsupportsystems.com>
2025-11-11 12:04:51 -0500

    Move assembly metadata to project file
    Assembly metadata attributes such as title, product, copyright, and version information were removed from AssemblyInfo.cs and added to the ConsumerApp.Net40.csproj project file. This centralizes assembly information management within the project file and reduces redundancy.

--- Source/ConsumerApp.Net40/Properties/AssemblyInfo.cs
+++ Source/ConsumerApp.Net40/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ConsumerApp.Net40")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsumerApp.Net40")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("03976e82-fa97-4d44-bb79-80254b00ccec")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
